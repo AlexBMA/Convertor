fc1da0dbc6f45f6013a0734f475e911dfe564bfb
AlexBMA <burghelea.alex@gmail.com>
2017-09-11 23:54:57 +0300

    update 5 update to the feetMeters activity

--- app/src/main/java/com/example/alex/distanceconvertor/FeetMeters.kt
+++ app/src/main/java/com/example/alex/distanceconvertor/FeetMeters.kt
@@ -3,6 +3,8 @@ package com.example.alex.distanceconvertor
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class FeetMeters : AppCompatActivity() {
 
@@ -12,10 +14,17 @@ class FeetMeters : AppCompatActivity() {
     }
 
     fun convertFeetToMeters(view: View) {
-
+        val editText = findViewById<EditText>(R.id.et_val_fm)
+        val meters = editText.text.toString().toDouble() * 0.3
+        val textView = findViewById<TextView>(R.id.tv_res_convert_f_m)
+        textView.text = "$meters " + getString(R.string.meters)
     }
 
     fun convertMetersToFeet(view: View) {
+        val editText = findViewById<EditText>(R.id.et_val_fm)
+        val feet = editText.text.toString().toDouble() * 3.28
+        val textView = findViewById<TextView>(R.id.tv_res_convert_f_m)
+        textView.text = "$feet " + getString(R.string.feet)
 
     }
 }
